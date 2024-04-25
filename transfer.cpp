30cc8d446b28179c5edeeb6e76d250b311673335
PoomNattapoom <rockyaggi@gmail.com>
2024-04-25 23:38:55 +0700

    dk

---
+++ transfer.cpp
@@ -0,0 +1,17 @@
+/* This Program Intended to transfer a number from numeric to english alphabetic */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  long long int num;
+  cin >> num; // 555
+
+  while (true)
+  {
+    if (num /)
+  }
+
+  return 0;
+}
